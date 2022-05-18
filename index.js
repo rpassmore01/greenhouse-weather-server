d6d318a225bbbd97f5d48dd6b28b66f04962d74b
rpassmore01 <russellpassmore2005@gmail.com>
2022-05-17 22:23:06 -0400

    connected to database

--- index.js
+++ index.js
@@ -1,13 +1,51 @@
 const express = require("express");
+const { Pool, Client } = require("pg");
+
+const credentials = {
+  user: "russell",
+  host: "localhost",
+  database: "greenhouseData",
+  password: "Dh*&36Gs",
+  port: 5432,
+};
+
+try {
+  async function poolDemo() {
+    const pool = new Pool(credentials);
+    const now = await pool.query("SELECT NOW()");
+    await pool.end();
+
+    return now.rows[0];
+  }
+
+  async function clientDemo() {
+    const client = new Client(credentials);
+    await client.connect();
+    const now = await client.query("SELECT NOW()");
+    await client.end();
+
+    return now.rows[0];
+  }
+
+  app.get("/", async (req, res) => {
+    const pool = await poolDemo();
+    const client = await clientDemo();
+
+    res.status(200).json({
+      pool: pool,
+      client: client,
+    });
+  });
+} catch (err) {
+  app.get("/", (req, res) => {
+    res.status(500).json({
+      message: err,
+    });
+  });
+}
 
 const app = express();
 const port = 3004;
-
-app.get("/", (req, res) => {
-  res.status(200).json({
-    message: "Successfully requested the root url.",
-  });
-});
 
 app.listen(port, () => {
   console.log(`server started on ${port} `);
